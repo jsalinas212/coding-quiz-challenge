9026ef484b688e1bb40fb3254b5562abf89ad959
Jorge A Salinas Jr <asalinas212@outlook.com>
2021-12-12 16:50:25 -0600

    Updated answer button and start button IDs, created questionObj as question template.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,37 @@
 // Pull elements by IDs and store in variables
-// Store questions in an array
+var quizScoreBoardEl = document.getElementById("#q-scoreboard");
+var quizTimerEl = document.getElementById("#q-time");
+var questionHeadlinEl = document.getElementById("#q-headline");
+var quizStartEl = document.getElementById("#q-start");
+var questionBodyEl = document.getElementById("#q-body");
+var answerListEl = document.getElementById("#q-alist");
+var cAnswerEl = document.getElementById("#c-answer");
+var wrongAnswerEl1 = document.getElementById("#w-answer1");
+var wrongAnswerEl2 = document.getElementById("#w-answer2");
+var wrongAnswerEl3 = document.getElementById("#w-answer3");
+
+// Create question object
+var questionObj = {
+    questions:  [
+        "Which HTMl element do we link our JavaScript file in?",
+        "Which is the correct way to select an element by its ID?",
+        "How do you output data to the console?",
+    ],
+    wrongAnswers:  [
+        "",
+        "",
+        "",
+        ""
+    ],
+    correctAnswers: [
+        "",
+        "",
+        ""
+    ]
+};
 // Store answers in arrays and link answers to questions
 // Event handlers for buttons
 // Functions for handling tasks
 // Build timer
+// Store score in local storage
+// Build scoreboard
