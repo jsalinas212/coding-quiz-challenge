b061dc06465d51727327e6268ada75ede63cf96b
Jorge A Salinas Jr <asalinas212@outlook.com>
2021-12-12 17:16:14 -0600

    added object properties, added functions to handle answer eval, quiz start, and next question, and added event handler for quiz start button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,73 @@
 // Pull elements by IDs and store in variables
-var quizScoreBoardEl = document.getElementById("#q-scoreboard");
-var quizTimerEl = document.getElementById("#q-time");
-var questionHeadlinEl = document.getElementById("#q-headline");
-var quizStartEl = document.getElementById("#q-start");
-var questionBodyEl = document.getElementById("#q-body");
-var answerListEl = document.getElementById("#q-alist");
-var cAnswerEl = document.getElementById("#c-answer");
-var wrongAnswerEl1 = document.getElementById("#w-answer1");
-var wrongAnswerEl2 = document.getElementById("#w-answer2");
-var wrongAnswerEl3 = document.getElementById("#w-answer3");
+
+// Header elements
+var quizScoreBoardEl = document.querySelector("#q-scoreboard");
+var quizTimerEl = document.querySelector("#q-time");
+
+// Question elements
+var questionHeadlinEl = document.querySelector("#q-headline");
+var quizStartEl = document.querySelector("#q-start");
+var questionBodyEl = document.querySelector("#q-body");
+
+// Answer elements
+var answerListEl = document.querySelector("#q-alist");
+var cAnswerEl = document.querySelector("#c-answer");
+var wrongAnswerEl1 = document.querySelector("#w-answer1");
+var wrongAnswerEl2 = document.querySelector("#w-answer2");
+var wrongAnswerEl3 = document.querySelector("#w-answer3");
+
+// Footer element
+var footerContentEl = document.querySelector("footer");
 
 // Create question object
 var questionObj = {
+    // Questions array
     questions:  [
-        "Which HTMl element do we link our JavaScript file in?",
+        "Between which HTMl elements can we link our JavaScript file in?",
         "Which is the correct way to select an element by its ID?",
-        "How do you output data to the console?",
+        "How do you output data to the console?"
     ],
+    // Wrong answers array
     wrongAnswers:  [
         "",
         "",
         "",
         ""
     ],
+    // Correct answers array
     correctAnswers: [
-        "",
+        "The <head> or <body> HTML tags",
         "",
         ""
-    ]
+    ],
+    // Question order function
+    questionOrder: {
+
+    }
 };
-// Store answers in arrays and link answers to questions
-// Event handlers for buttons
+
 // Functions for handling tasks
+
+// Evaluate answer to see if correct or wrong
+var evaluateAnswer = function(event) {
+
+}
+
+// Serve up next question
+var nextQuestion = function(event) {
+
+}
+
+// Start the quiz
+var startQuiz = function(event) {
+    console.log("The quiz has started.");
+}
+
 // Build timer
-// Store score in local storage
+
 // Build scoreboard
+
+// Store score in local storage
+
+// Event handlers for buttons
+quizStartEl.addEventListener("click", startQuiz);
