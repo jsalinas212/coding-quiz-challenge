3e12cf201134e4f89392ee4871410d8f0cf95d4b
Jorge A Salinas Jr <asalinas212@outlook.com>
2021-12-12 15:40:54 -0600

    added element IDs, started adding variables for elements, added answer list html and styled.

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+// Pull elements by IDs and store in variables
+// Store questions in an array
+// Store answers in arrays and link answers to questions
+// Event handlers for buttons
+// Functions for handling tasks
+// Build timer
